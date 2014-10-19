78d2e8b7824612647d43229b573d7e52e6087389
Charlesworth <cochrane26@gmail.com>
2014-10-19 16:04:36 +0100

    styles now auto add from resoureces folder. dates now sort in index list

--- local.rb
+++ local.rb
@@ -46,7 +46,7 @@ class WebSpeak
       end
     end
 
-    Page_date[@@date] = a.chomp(".txt")		
+    Page_date[a.chomp(".txt")] = @@date
 		
     erb = ERB.new(File.read('resources/views/page_template.erb'))
     output_file = File.new("Website/Pages/" + a.chomp(".txt") + ".html", "w")
@@ -66,7 +66,7 @@ class IndexPage
     @@index_options = file.readlines[0..2]
     if @@index_options[1].include?("Yes") #If display title = yes
       title = @@index_options[0].split(': ')[1]
-      HTML << "<h1 style='padding-left: 0.5em;'>" + title + "</h1>"
+      #Blog_title << title
     end
 		
 		if (@@index_options[2][26].to_i > Pages.length)	
@@ -76,33 +76,39 @@ class IndexPage
 		end
 		
     for i in 0..(@@index_length - 1)
-      HTML << "<a href='./Pages/#{Pages[i]}.html'>#{Pages[i]}</a>"
+      Page_content << "<a href='./Pages/#{Pages[i]}.html'>#{Pages[i]} : #{Page_date[Pages[i]]}</a><br />"
     end 
 
+		erb = ERB.new(File.read('resources/views/index_template.erb'))
     output_file = File.new("Website/index.html", "w")
-
-    HTML.each do |x|
-      output_file.puts x + "<br />"
-    end
-
+    output_file.puts erb.result()
     output_file.close
-    HTML.clear
+
+    Page_content.clear
   end
 end
 
 InputFilesIndex = Dir.entries(Dir.pwd + "/Input").reject{|entry| entry == "." || entry == ".."}.sort
-HTML = Array.new
 Pages = Array.new
 Page_content = String.new
 Page_date = Hash.new
+#Blog_title = String.new
 
-OutputDirectories = ["Website", "Website/Pictures", "Website/Pages", "Website/Style"]
-OutputDirectories.each {|x| if (Dir.exist?(x) == false) then Dir.mkdir(x) end}
+Output_directories = ["Website", "Website/Pictures", "Website/Pages", "Website/Style"]
+Output_directories.each {|x| if (Dir.exist?(x) == false) then Dir.mkdir(x) end}
+Style_import = ["bootstrap.min.css", "sticky-footer-navbar.css"]
+Style_import.each {|y| FileUtils.cp("resources/Style/css/#{y}", "Website/Style/#{y}")}
+
+#------------------
+file = File.open("resources/Index_options.txt", "r")
+Blog_title = file.readlines[0].split(': ')[1]
+puts Blog_title
+#------------------
 
 InputFilesIndex.each do |current_file|
 	if current_file.include? ".txt"
     Pages << current_file.chomp(".txt")
-    WebSpeak.new.tohtml(current_file)
+    #WebSpeak.new.tohtml(current_file)
   elsif current_file.include? ".jpg" or current_file.include? ".png"
     FileUtils.cp("Input/#{current_file}", "Website/Pictures/#{current_file}")
 	elsif current_file.include? ".md"
@@ -111,6 +117,8 @@ InputFilesIndex.each do |current_file|
     puts "file #{current_file} not recognised, please look at supported file types"
 	end
 end
+
+Pages.each {|page| WebSpeak.new.tohtml(page + ".txt")}
 
 puts "builds complete, making index"
 IndexPage.new.make
