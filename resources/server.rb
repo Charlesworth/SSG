3089d7fe6b8c279956c7fde1b1a1619386f95aaa
Charlesworth <cochrane26@gmail.com>
2014-11-02 18:55:39 +0000

    updating the server.rb to become the command page, not finished

--- resources/server.rb
+++ resources/server.rb
@@ -1,13 +1,33 @@
 require 'sinatra'
 require 'launchy'
 
-Launchy.open("http://localhost:4567/Start")
+Launchy.open("http://localhost:4567/")
+
+
 #open the init erb form
-#take in the answers to a txt file
+#load the presets from the local options file
+
+
+
+#present options and build button
+#take in the answers to options.txt file if changed
 #close the page or put up a "website made, can be found in #{folder} folder
 
 get '/' do
-	"Hello, World!"
+	  if !File.exist?("Index_options.txt")
+			output_file = File.new("Index_options.txt", "w")
+			output_file.puts ["Blog Name: My Blog", "Name Displayed in Title: Yes", "Links on each index page: 5"]
+			output_file.close
+		end
+	
+		file = File.open("Index_options.txt", "r")
+		index_options = file.readlines[0..2]
+		
+		Title = index_options[0].split(': ')[1]
+		
+		A = "Hello, is it me your looking for?"
+		
+		erb :test
 end
 
 get '/about' do
@@ -50,6 +70,14 @@ post '/form' do
 	# "you posted something"
 	output =  params[:message] + " " + params[:fuck]
 	"You said '#{params[:fuck]} #{params[:message]}', Well #{output}"
+	#if params[:message].empty? "no mess no fuss!"
+end
+
+post '/form1' do
+	# "you posted something"
+	output =  params[:message] + " " + params[:fuck]
+	"You said '#{params[:fuck]} #{params[:message]}', Well #{output}"
+	
 	#if params[:message].empty? "no mess no fuss!"
 end
 
